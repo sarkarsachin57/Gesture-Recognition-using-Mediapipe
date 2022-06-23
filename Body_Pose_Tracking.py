8b68888108e6730eb1db7ff7f032fb3e3412ca03
Sachin Sarkar <83460431+sarkarsachin57@users.noreply.github.com>
2022-06-23 15:40:54 +0530

    Create Body_Pose_Tracking.py

---
+++ Body_Pose_Tracking.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+
+vid = cv2.VideoCapture(0)
+
+pose = mp.solutions.pose.Pose()
+
+draw_fn = mp.solutions.drawing_utils
+
+while vid.isOpened():
+    
+    ret , img = vid.read()
+    
+    if ret == False :
+        print('Can not continue the video or webcam.')
+        break
+    
+    results = pose.process(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    
+    if results.pose_landmarks:
+            
+        draw_fn.draw_landmarks(img, results.pose_landmarks, mp.solutions.pose.POSE_CONNECTIONS)
+    
+    cv2.imshow('Pose Tracking',img)
+    
+    if cv2.waitKey(1) in [27,13,ord('q')]:
+        break
+        
+vid.release()
+cv2.destroyAllWindows()
